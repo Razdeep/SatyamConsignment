c3d42d31f9f1723f57cb7fa53317450f8f26fee0
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-07-04 15:51:49 +0530

    Restructured miscellaneous files
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/satyamconsignment/misc/Rrc.java
+++ src/satyamconsignment/misc/Rrc.java
@@ -1,77 +1,34 @@
 package satyamconsignment.misc;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.stage.Stage;
 
 public class Rrc {
-    public void showAlert(String message)
-    {
-        Alert alert= new Alert(AlertType.ERROR);
+
+    public static void showAlert(String message) {
+        Alert alert = new Alert(AlertType.ERROR);
         alert.setContentText(message);
         alert.show();
     }
-    public void showAlert(String message,int choice)
-    {
-        Alert alert;
-        switch(choice)
-        {
+
+    private static AlertType getAlertTypeFromCustomValue(int choice) {
+        switch (choice) {
             case 0:
-            alert= new Alert(AlertType.ERROR);
-            alert.setContentText(message);
-            alert.show();
-            break;
+                return AlertType.ERROR;
             case 1:
-            alert= new Alert(AlertType.INFORMATION);
-            alert.setContentText(message);
-            alert.show();
-            break;
+                return AlertType.INFORMATION;
             case 2:
-            alert= new Alert(AlertType.WARNING);
-            alert.setContentText(message);
-            alert.show();
-            break;
+                return AlertType.WARNING;
             case 3:
-            alert= new Alert(AlertType.CONFIRMATION);
-            alert.setContentText(message);
-            alert.show();
-            break;
+                return AlertType.CONFIRMATION;
             default:
-            showAlert("Programming Error. Select 1->error 2->info 3->warn 4->confirm");
-            break;
+                showAlert("Programming Error. Select 1->error 2->info 3->warn 4->confirm");
         }
+        return AlertType.NONE;
     }
-    public void loadWindow(String loc,String title)
-    {
-        try {
-            Parent parent=FXMLLoader.load(getClass().getResource(loc));
-            Stage stage=new Stage();
-            stage.setScene(new Scene(parent));
-            stage.setTitle(title);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(Rrc.class.getName()).log(Level.SEVERE, null, ex);
-            this.showAlert(ex.toString());
-        }
-    }
-    public void loadWindowMaximized(String loc,String title)
-    {
-        try {
-            Parent parent=FXMLLoader.load(getClass().getResource(loc));
-            Stage stage=new Stage();
-            stage.setScene(new Scene(parent));
-            stage.setTitle(title);
-            stage.setMaximized(true);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(Rrc.class.getName()).log(Level.SEVERE, null, ex);
-            this.showAlert(ex.toString());
-        }
+
+    public static void showAlert(String message, int choice) {
+        Alert alert = new Alert(getAlertTypeFromCustomValue(choice), message);
+        alert.show();
     }
 }
