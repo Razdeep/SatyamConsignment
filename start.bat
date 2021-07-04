cbfdecce9bae2754a8747cd3e4b9953acd9c4015
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-07-04 09:10:46 +0530

    Added batch script for launching in windows with Java 11
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

---
+++ start.bat
@@ -0,0 +1,8 @@
+java --module-path "C:\Users\ADMIN\javafx-sdk-11.0.2\lib" ^
+--add-modules javafx.controls,javafx.base,javafx.graphics,javafx.fxml ^
+--add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED ^
+--add-exports=javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED ^
+--add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED ^
+--add-exports=javafx.base/com.sun.javafx.binding=ALL-UNNAMED ^
+--add-exports=javafx.graphics/com.sun.javafx.stage=ALL-UNNAMED ^
+-jar build/libs/SatyamConsignment.jar
