fecef474426c614a81e2e41b8dc6b36e4e0320b2
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-12-06 09:11:41 +0530

    fix: setting java version to 11 before application startup
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- start.bat
+++ start.bat
@@ -1,3 +1,6 @@
+set JAVA_HOME="C:\Program Files\Java\jdk-11.0.11\lib"
+set PATH="C:\Program Files\Java\jdk-11.0.11\bin"
+
 java --module-path "C:\Users\ADMIN\javafx-sdk-11.0.2\lib" ^
 --add-modules javafx.controls,javafx.base,javafx.graphics,javafx.fxml ^
 --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED ^
@@ -5,4 +8,4 @@ java --module-path "C:\Users\ADMIN\javafx-sdk-11.0.2\lib" ^
 --add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED ^
 --add-exports=javafx.base/com.sun.javafx.binding=ALL-UNNAMED ^
 --add-exports=javafx.graphics/com.sun.javafx.stage=ALL-UNNAMED ^
--jar build/libs/SatyamConsignment.jar
+-jar SatyamConsignment.jar
