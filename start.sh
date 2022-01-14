f493c79cd161929938b8897d2c6a37ddce6628f4
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-01-14 17:47:06 +0530

    fix: start.sh fixed with export modules to unnamed module
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-java --module-path "/home/rajdeep/javafx-sdk-11.0.2/lib" --add-modules javafx.controls,javafx.base,javafx.graphics,javafx.fxml -jar build/libs/SatyamConsignment.jar
+java --module-path "/home/rajdeep/javafx-sdk-11.0.2/lib" \
+	--add-modules javafx.controls,javafx.base,javafx.graphics,javafx.fxml \
+	--add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED \
+	--add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED \
+	--add-exports javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED \
+	--add-exports javafx.base/com.sun.javafx.binding=ALL-UNNAMED \
+	--add-exports javafx.graphics/com.sun.javafx.stage=ALL-UNNAMED \
+	-jar build/libs/SatyamConsignment.jar
