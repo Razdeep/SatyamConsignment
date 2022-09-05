9703b7800c14961bd9a396b90fbf612fdfc592ae
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-05 07:57:21 +0530

    feat: using environment variables in start.sh to specify openjfx path
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-java --module-path "/home/rajdeep/javafx-sdk-11.0.2/lib" \
+#!/usr/bin/bash
+
+if [[ -z "$PATH_TO_FX" ]]; then
+	echo "Please the environment variable PATH_TO_FX and try to rerun"
+	exit 1
+fi
+
+java --module-path $PATH_TO_FX \
 	--add-modules javafx.controls,javafx.base,javafx.graphics,javafx.fxml \
 	--add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED \
 	--add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED \
