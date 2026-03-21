fa74b61bf5144dce149799bf99a5aedafb25429e
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2026-03-21 17:12:20 +0530

    feat: converted to gradle groovy to kotlin
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

---
+++ Justfile
@@ -0,0 +1,2 @@
+format:
+    ./gradlew spotlessApply
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "SatyamConsignment"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
