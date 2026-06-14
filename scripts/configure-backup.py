affaa330d3453e57f51b696f4f0925caf9eec5b5
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2026-06-14 10:07:48 +0530

    feat: init git repo added in configure-backup.py
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- scripts/configure-backup.py
+++ scripts/configure-backup.py
@@ -1,5 +1,7 @@
 import platform
 import re
+import subprocess
+from pathlib import Path
 
 year = str(input("Enter financial year in the format like (2026-27): "))
 
@@ -9,15 +11,7 @@ def check_valid_financial_year(year: str):
         return False
     start_year = year.split("-")[0][2:]
     end_year = year.split("-")[1]
-    print(f"start_year: {start_year}, end_year: {end_year}")
     return int(start_year) + 1 == int(end_year)
-
-
-if not check_valid_financial_year(year):
-    print("Please enter a valid financial year in the format like (2026-27)")
-    exit(0)
-
-os_name = platform.system()
 
 
 def replace_in_template(input_file, output_file, placeholder, replacement):
@@ -46,6 +40,36 @@ def generate_batch_scripts():
         "upload.template.bat", "upload.bat", "{{PLACEHOLDER_YEAR}}", year
     )
 
+
+def init_github_repository():
+    repo_name = f"Satyam-Database-{year}"
+    if (Path(repo_name) / ".git").is_dir():
+        print(f"{repo_name} already exists, so skipping clone")
+        return
+    github_username = "drcbabu81"
+    repo_url = f"git@github.com:{github_username}/{repo_name}.git"
+
+    try:
+        subprocess.run(
+            ["git", "clone", repo_url], check=True, capture_output=True, text=True
+        )
+
+        if (Path(repo_name) / ".git").is_dir():
+            print("Clone successful")
+        else:
+            print("Git returned success but .git directory is missing")
+
+    except subprocess.CalledProcessError as e:
+        print("Clone failed")
+        print(e.stderr)
+
+
+if not check_valid_financial_year(year):
+    print("Please enter a valid financial year in the format like (2026-27)")
+    exit(0)
+
+os_name = platform.system()
+init_github_repository()
 
 if os_name == "Windows":
     print("Running on Windows")
